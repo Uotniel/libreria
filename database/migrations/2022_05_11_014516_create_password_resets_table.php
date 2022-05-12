985b526970dc4944f95b4d2d258e49b092d5aac2
Otniel Garcia <uotniel@gmail.com>
2022-05-11 22:47:28 -0500

    Create a new model of migrations and create s user sedder.

--- database/migrations/2022_05_11_014516_create_password_resets_table.php
+++ database/migrations/2022_05_11_014516_create_password_resets_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreatePasswordResetsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,13 @@ return new class extends Migration
      */
     public function up()
     {
-      Schema::create('categories', function (Blueprint $table){
-        $table->engine="InnoDB";
-        $table->bigIncrements('id');
-        $table->string('name');
-        $table->timestamps();
-      });
+        Schema::create('password_resets', function (Blueprint $table) {
+            $table->string('email')->index();
+            $table->string('token');
+            $table->timestamp('created_at')->nullable();
+        });
     }
+
     /**
      * Reverse the migrations.
      *
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down()
     {
-      Schema::dropIfExists('categories');
+        Schema::dropIfExists('password_resets');
     }
-};
+}
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $users=array([
+          'name' => 'admin',
+          'email' => 'admin2@db.com',
+          'email_verified_at' => now(),
+          'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+          'remember_token' => Str::random(10),
+          ],
+          [
+          'name' => 'admin',
+          'email' => 'admin1@db.com',
+          'email_verified_at' => now(),
+          'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+          'remember_token' => Str::random(10),
+          ]
+        );
+
+        foreach ($users as $user) {
+        //  dd($user);
+          User::create($user);
+        }
+    }
+}
